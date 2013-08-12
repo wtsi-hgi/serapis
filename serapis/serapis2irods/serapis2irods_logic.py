cd2aa3ed80dd385b48a829bba9195dfc4b293275
IrinaColgiu <ic4@sanger.ac.uk>
2013-08-12 15:48:25 +0100

    - Changed to use the icommands instead of the API also for the metadata.

--- serapis/serapis2irods/serapis2irods_logic.py
+++ serapis/serapis2irods/serapis2irods_logic.py
@@ -2,9 +2,11 @@
 from serapis import db_model_operations
 import convert_mdata
 
-def gather_mdata(file_to_submit):
-    user_id = db_model_operations.retrieve_sanger_user_id(file_to_submit.id)
-    subm_date = db_model_operations.retrieve_submission_date(file_to_submit.id)
+def gather_mdata(file_to_submit, user_id=None, submission_date=None):
+    if user_id == None:
+        user_id = db_model_operations.retrieve_sanger_user_id(file_to_submit.id)
+    if submission_date == None:
+        subm_date = db_model_operations.retrieve_submission_date(file_to_submit.id)
     print "SUBMISSION DATEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE: ", subm_date
     if hasattr(file_to_submit, 'file_reference_genome_id') and getattr(file_to_submit, 'file_reference_genome_id') != None:
         ref_genome = db_model_operations.retrieve_reference_by_md5(file_to_submit.file_reference_genome_id)
