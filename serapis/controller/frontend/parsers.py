b01b3b75aa807dcd10dc8bd22cf41667593d58a3
IrinaColgiu <ic4@sanger.ac.uk>
2014-04-04 15:26:41 +0100

    Added a new module - to be implemented - for parsing other formats.

---
+++ serapis/controller/frontend/parsers.py
@@ -0,0 +1,49 @@
+
+from rest_framework.parsers import BaseParser
+import zlib
+
+#from Celery_Django_Prj import settings
+
+
+class GZIPParser(BaseParser):
+    """
+    Plain text parser.
+    """
+
+    media_type = 'application/gzip'
+    
+    def parse(self, stream, media_type=None, parser_context=None):
+        """
+        Simply return a string representing the body of the request.
+        """
+        pass
+#
+
+##        decompressed_data=zlib.decompress(stream.read())
+##        return decompressed_data
+#        #return stream.read()
+#
+#        #str_object1 = open('compressed_file', 'rb').read()
+#        
+#        print "VARS -----------------", vars(stream)
+#        print "STREAM FROM PARSE FCT: ", stream
+#        parser_context = parser_context or {}
+#        request = parser_context['request']
+#        encoding = parser_context.get('encoding')
+#        
+#        print "REQUEST EXTRACTED:::::::::::::::::", request
+#        print "VARS OF REQUEST::::::::::::::::::::::::::::", vars(request)
+#        return zlib.decompress(request.get())
+##        f = open('my_recovered_log_file', 'wb')
+##        f.write(str_object2)
+##        f.close()
+#
+#
+##        from StringIO import StringIO
+##        import gzip
+##
+##        buff = StringIO(stream.read())
+##        deflatedContent = gzip.GzipFile(fileobj=buff)
+##    
+##        return deflatedContent.read()
+
