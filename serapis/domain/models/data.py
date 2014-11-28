dd83f4f0fd73954fd8d765e46c621c88526f4f4f
irinaColgiu <irina.colgiu@gmail.com>
2014-11-28 17:20:01 +0000

    - Added unittest for data.py - Added some more methods implementations to the methods schetched in data.py - Fixed some small bugs in data.py and fct it calls

--- serapis/domain/models/data.py
+++ serapis/domain/models/data.py
@@ -3,9 +3,11 @@ This module is implementing the functionality related to the data that one store
 where by data we refer to the actual information content of a file.
 """
 
-import data_entities
+import data_entity
 from serapis.com import constants
-from serapis.controller import exceptions
+from multimethods import multimethod
+from serapis.seqscape import queries as seqsc
+from serapis.com import wrappers
 
 
 class DataProcessing(object):
@@ -16,7 +18,6 @@ class DataProcessing(object):
                                                             # hgi_bam_improvement -> or this can be synonym with a list of processings...
                                                             # spatial_filter,
                                                             # merged_lanelets, not sure what type they are yet
-
 
 class GenomeRegions(object):
     """ Contains a list of chromosomes or other details regarding which regions the data contains"""
@@ -30,16 +31,28 @@ class Data(object):
         self.processing = processing
         self.pmid_list = pmid_list
         self.security_level = security_level
-        self.studies = data_entities.StudyCollection(study_set=studies)  # This has the type StudyCollection
+        self.studies = data_entity.StudyCollection(study_set=studies)  # This has the type StudyCollection
+
+    def add_study(self, study):
+        return self.studies.add(study)
 
     def add_or_update_study(self, study):
         return self.studies.add_or_update(study)
 
+    def add_all_studies(self, studies):
+        return self.studies.add_all(studies)
+
+    def remove_all_studies(self):
+        return self.studies.remove_all()
+
     def remove_study(self, study):
         return self.studies.remove(study)
 
-    def remove_study_by_name(self, study_name):
-        return self.studies.remove_by_name(study_name)
+    def remove_study_by_name(self, name):
+        return self.studies.remove_by_name(name)
+
+    def remove_study_by_accession_number(self, accession_number):
+        return self.studies.remove_by_accession_number(accession_number)
 
     def is_field_empty(self, field):
         return not (hasattr(self, field) and getattr(self, field) is not None)
@@ -78,31 +91,50 @@ class Data(object):
     #         return task_id
     #         #register_task_id
 
-    def update(self, new_data):
-        pass
+    # @multimethod(data_entity.Study)
+    # def lookup_study_in_seqscape(self, study):
+    #     pass
+    #
+    # @multimethod(str, str)
+    # def lookup_study_in_seqscape(self, id_val, id_type):
+    #     pass
+
+    @classmethod
+    def get_metadata_for_studies_from_seqscape(cls, ids_as_tuples):
+        #study_ids_as_tuples = [study.export_identifier_as_tuple() for study in studies]
+        ids = set([tup[1] for tup in ids_as_tuples])
+        id_types = set([tup[0] for tup in ids_as_tuples])
+        if len(id_types) == 1:
+            result_studies = seqsc.query_all_studies_as_batch(ids, list(id_types)[0])
+        else:
+            result_studies = seqsc.query_all_studies_individually(ids_as_tuples)
+        return result_studies
 
 
 class DNAData(Data):
     def __init__(self, pmid_list, security_level=constants.SECURITY_LEVEL_2, processing=None, coverage_list=None,
                  sorting_order=None, library_strategy=None, library_source=None, genomic_regions=GenomeRegions()):
         super(DNAData, self).__init__(processing, pmid_list, security_level)
-        self.libraries = data_entities.LibraryCollection(strategy=library_strategy, source=library_source)
-        self.samples = data_entities.SampleCollection()
+        self.libraries = data_entity.LibraryCollection(strategy=library_strategy, source=library_source)
+        self.samples = data_entity.SampleCollection()
         self.genomic_regions = genomic_regions  # this has GenomeRegions as type
         self.sorting_order = sorting_order
         self.coverage_list = coverage_list
 
     def add_sample(self, sample):
-        return self.samples.add_sample(sample)
+        return self.samples.add(sample)
+
+    def add_or_update_sample(self, sample):
+        return self.samples.add_or_update(sample)
 
     def add_all_samples(self, sample_list):
-        return self.samples.add_all_samples(sample_list)
+        return self.samples.add_all(sample_list)
 
     def add_library(self, library):
-        return self.libraries.add_library(library)
+        return self.libraries.add(library)
 
     def add_all_libraries(self, library_list):
-        return self.libraries.add_all_libraries(library_list)
+        return self.libraries.add_all(library_list)
 
     def remove_sample(self, sample):
         return self.samples.remove(sample)
@@ -110,11 +142,66 @@ class DNAData(Data):
     def remove_sample_by_name(self, name):
         return self.samples.remove_by_name(name)
 
+    def remove_sample_by_accession_number(self, accession_number):
+        return self.samples.remove_by_accession_number(accession_number)
+
+    def remove_library_by_name(self, name):
+        return self.libraries.remove_by_name(name)
+
     def remove_all_samples(self):
-        pass
+        return self.samples.remove_all()
 
     def remove_all_libraries(self):
-        pass
+        return self.libraries.remove_all()
+
+    @classmethod
+    @wrappers.check_args_not_none
+    def get_metadata_for_samples_from_seqscape(cls, ids_as_tuples):
+        #ids_as_tuples = [s.export_id_as_tuple() for s in samples]
+        ids = set([tup[1] for tup in ids_as_tuples])
+        id_types = set([tup[0] for tup in ids_as_tuples])
+        if len(id_types) == 1:
+            result = seqsc.query_all_samples_as_batch(ids, list(id_types)[0])
+        else:
+            result = seqsc.query_all_samples_individually(ids_as_tuples)
+        return result
+
+
+    @classmethod
+    @wrappers.check_args_not_none
+    def get_metadata_for_libraries_from_seqscape(cls, ids_as_tuples):
+        #ids_as_tuples = [l.export_id_as_tuple() for l in libraries]
+        ids = set([tup[1] for tup in ids_as_tuples])
+        id_types = set([tup[0] for tup in ids_as_tuples])
+        if len(id_types) == 1:
+            result = seqsc.query_all_samples_as_batch(ids, list(id_types)[0])
+        else:
+            result = seqsc.query_all_samples_individually(ids_as_tuples)
+        return result
+
+
+    @classmethod
+    @wrappers.check_args_not_none
+    def get_metadata_for_studies_by_samples_from_seqscape(cls, sample_internal_ids):
+        #sample_ids = [s.internal_id for s in samples if s.internal_id is not None]
+        if not sample_internal_ids:
+            return []
+        studies = seqsc.query_all_studies_associated_with_samples(sample_internal_ids)
+        return studies
+
+
+    def collect_and_integrate_metadata_from_seqscape(self, samples, libraries, studies=None):
+        samples = self.get_metadata_for_samples_from_seqscape()
+        libraries = self.get_metadata_for_libraries_from_seqscape()
+        if samples:
+            studies = self.get_metadata_for_studies_from_seqscape()
+        else:
+            studies = self.get_metadata_for_studies_by_samples_from_seqscape()
+        # transform seqscape entities into json or smth usable here
+        # change json to data_entities. corresponding
+        # integrate them somehow into the existing lists of entities...merge?!
+        # ...? what now?
+
 
 
 class DNASequenceData(DNAData):
