7f11c287072484e4a5ddad77dd5976f7fab020eb
IrinaColgiu <ic4@sanger.ac.uk>
2016-09-19 16:33:57 +0100

    updated the filesystem storage API

--- serapis/storage/filesystem/lustre_storage.py
+++ serapis/storage/filesystem/lustre_storage.py
@@ -12,58 +12,46 @@ from serapis.storage.base import Storage
 from serapis.com import wrappers
 
 
-class LustreStorage(Storage):
-    
-      
-    def get_file_permissions(self):
+class FileSystemBasicAPI(Storage):
+
+    @classmethod
+    def get_permissions(cls, path):
         pass
-    
-    def get_dir_permissions(self):
+
+    @classmethod
+    def set_permissions(cls, path, permission):
         pass
-    
-    def set_file_permissions(self):
+
+    @classmethod
+    def upload(cls, src_path, dest_path):
+        """ This function uploads a file from a different FS, into this FS.
+            TODO: think of how it's going to work in practice for copying data over.
+        """
         pass
-    
-    def set_dir_permissions(self):
-        pass
-    
-    
-#     def upload_file(self):
-#         ''' This function uploads a file from a different FS, into this FS.'''
-#         pass
-#     
-#     def upload_dir(self):
-#         pass
-    
-    
-    def copy_file(self):
-        ''' This method copies a file within the same backend file system'''
-        raise NotImplementedError
-    
-    def copy_dir(self):
-        raise NotImplementedError
-    
-    
-    def move_file(self):
-        raise NotImplementedError
-    
-    def move_dir(self):
-        raise NotImplementedError
-    
-    def delete_file(self):
-        raise NotImplementedError
-    
-    def delete_dir(self):
-        raise NotImplementedError
-    
-    def make_dir(self):
+
+    @classmethod
+    def copy(cls, src_path, dest_path):
+        """ This method copies a file within the same backend file system"""
         raise NotImplementedError
 
     @classmethod
-    @wrappers.check_args_not_none
-    def list_dir(cls, path):
-        ''' Throws a ValueError if the dir doesn't exist or the path 
-            is not a dir or if the dir is empty. 
+    def move(cls, src_path, dest_path):
+        raise NotImplementedError
+
+    @classmethod
+    def delete(cls, path):
+        raise NotImplementedError
+
+
+class DirectoryAPI(FileSystemBasicAPI):
+
+    @classmethod
+    def create(cls, path):
+        raise NotImplementedError
+
+    @classmethod
+    def list_contents(cls, path):
+        """ Throws a ValueError if the dir doesn't exist or the path is not a dir or if the dir is empty.
             Returns the list of files and dirs from that dir.
             Parameters
             ----------
@@ -72,26 +60,25 @@ class LustreStorage(Storage):
             Returns
             -------
             a list of file names and directory names contained in the directory given as parameter
-        '''    
+        """
         return [f for f in os.listdir(path)]
     
     @classmethod
-    @wrappers.check_args_not_none
     def exists(cls, path):
         return os.path.exists(path)
     
     @classmethod
-    @wrappers.check_args_not_none
     def is_dir(cls, path):
         return os.path.isdir(path)
-    
+
+
+class FileAPI(FileSystemBasicAPI):
+
     @classmethod
-    @wrappers.check_args_not_none
     def is_file(cls, path):
         return os.path.isfile(path)
     
     @classmethod
-    @wrappers.check_args_not_none
     def _calculate_md5(cls, path, BLOCK_SIZE=1048576):
         fd = open(path, 'r')
         file_obj = fd
@@ -104,13 +91,11 @@ class LustreStorage(Storage):
         return md5_sum.hexdigest()
     
     @classmethod
-    @wrappers.check_args_not_none
-    def checksum_file(cls, path, checksum_type='md5'):
+    def calculate_checksum(cls, path, checksum_type='md5'):
         if checksum_type == 'md5':
             return cls._calculate_md5(path)
         else:
             raise NotImplementedError('Lustre API doesnt support at the moment other type of checksum')
-    
-    def get_size(self):
-        pass
+
+
 
