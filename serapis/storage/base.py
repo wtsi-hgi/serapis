5b303a8889bf4479db249392130f7f13d9f58a02
IrinaColgiu <ic4@sanger.ac.uk>
2016-09-19 16:42:06 +0100

    Updated basic storage api

--- serapis/storage/base.py
+++ serapis/storage/base.py
@@ -7,71 +7,61 @@ Created on Oct 26, 2014
 from serapis.com import constants
 
 
-class Storage(object):
+class StorageBasicAPI:
 
-    # TODO: make all these class methods
-    def get_file_permissions(self):
+    @classmethod
+    def get_permissions(cls, path):
         pass
-    
-    def get_dir_permissions(self):
+
+    @classmethod
+    def set_permissions(cls, permissions):
         pass
-    
-    def set_file_permissions(self):
-        pass
-    
-    def set_dir_permissions(self):
-        pass
-    
-    
-    def upload_file(self):
+
+    @classmethod
+    def upload(cls, src_path, dest_path):
         """ This function uploads a file from a different FS, into this FS."""
         pass
 
-    def upload_dir(self):
-        pass
-
-    
-    def copy_file(self):
+    @classmethod
+    def copy(cls, src_path, dest_path):
         """ This method copies a file within the same storage system"""
         pass
-    
-    def copy_dir(self):
-        """ This method copies a directory from one location to another within the same storage system"""
-        pass
-    
-    def move_file(self):
-        pass
-    
-    def move_dir(self):
-        pass
-    
-    def delete_file(self):
-        pass
-    
-    def delete_dir(self):
-        pass
-    
-    def make_dir(self):
-        pass
-    
-    def list_dir(self):
-        pass
-    
-    def exists(self, path):
-        pass
-    
-    def is_dir(self):
-        pass
-    
-    def is_file(self):
-        pass
-    
 
-    def checksum_file(self, path, checksum_type='md5'):
+    @classmethod
+    def move(cls, src_path, dest_path):
+        pass
+
+    @classmethod
+    def delete(cls, path):
+        pass
+
+    @classmethod
+    def exists(cls, path):
+        pass
+
+
+class DirectoryAPI(StorageBasicAPI):
+
+    @classmethod
+    def create(cls, path):
+        pass
+
+    @classmethod
+    def list_contents(cls, path):
+        pass
+
+    @classmethod
+    def is_dir(cls, path):
+        pass
+
+
+class FileAPI(StorageBasicAPI):
+
+    @classmethod
+    def is_file(cls, path):
+        pass
+
+    @classmethod
+    def calculate_checksum(cls, path, checksum_type='md5'):
         pass
     
-    def checksum_dir(self, path, checksum_type='md5'):
-        pass
-    
-    def get_size(self):
-        pass
