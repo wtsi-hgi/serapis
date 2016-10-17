9a2fc5536519000dec8c4b0c945a6195e492495c
IrinaColgiu <ic4@sanger.ac.uk>
2016-10-17 15:03:13 +0100

    Added eq and hash() fcts

--- serapis/domain/models/data.py
+++ serapis/domain/models/data.py
@@ -14,9 +14,10 @@ class DataProcessing(object):
 
     def __init__(self, processing_list=[]):
         self.processing_list = processing_list  # items in this list can be:
-                                                            # hgi_bam_improvement -> or this can be synonym with a list of processings...
-                                                            # spatial_filter,
-                                                            # merged_lanelets, not sure what type they are yet
+        # hgi_bam_improvement -> or this can be synonym with a list of processings...
+        # spatial_filter,
+        # merged_lanelets, not sure what type they are yet
+
 
 class GenomeRegions(object):
     """ Contains a list of chromosomes or other details regarding which regions the data contains"""
@@ -32,6 +33,15 @@ class Data(object):
         self.security_level = security_level
         self.studies = data_entity.StudyCollection(study_set=studies)  # This has the type StudyCollection
 
+    def __eq__(self, other):
+        if type(other) != type(self):
+            return False
+        return self.pmid_list == other.pmid_list and self.studies == other.studies and \
+               self.processing == other.processing and self.security_level == other.security_level
+
+    def __hash__(self):
+        return hash(self.pmid_list) + hash(self.security_level)
+
     def has_enough_metadata_for_submission(self):
         # TODO: re-write it so that "enough" is defined somewhere externally in a config file or so
         pass
@@ -44,19 +54,34 @@ class Data(object):
 class GenotypingData(Data):
     def __init__(self, processing, pmid_list, studies, security_level=constants.SECURITY_LEVEL_2, genome_reference=None,
                  disease_or_trait=None, nr_samples=None, ethnicity=None):
-        super(GWASData, self).__init__(processing, pmid_list, studies, security_level)
+        super(GenotypingData, self).__init__(processing, pmid_list, studies, security_level)
         self.genome_reference = genome_reference
         self.disease_or_trait = disease_or_trait
         self.nr_samples = nr_samples
         self.ethnicity = ethnicity
 
+    def __eq__(self, other):
+        return super(GenotypingData, self).__eq__(other) and self.genome_reference == other.genome_reference and \
+               self.disease_or_trait == other.disease_or_trait and self.nr_samples == other.nr_samples and \
+               self.ethnicity == other.ethnicity
+
+    def __hash__(self):
+        return super(GenotypingData, self).__hash__() + hash(self.genome_reference) + \
+               hash(self.disease_or_trait) + hash(self.nr_samples)
+
 
 class GWASData(GenotypingData):
-
     def __init__(self, processing, pmid_list, studies, security_level=constants.SECURITY_LEVEL_2, genome_reference=None,
                  disease_or_trait=None, nr_samples=None, ethnicity=None, study_type=None):
-        super(GWASData, self).__init__(processing, pmid_list, studies, security_level, genome_reference, disease_or_trait, nr_samples, ethnicity)
-        self.study_type = study_type    # Can be: case-control, trio, etc.
+        super(GWASData, self).__init__(processing, pmid_list, studies, security_level, genome_reference,
+                                       disease_or_trait, nr_samples, ethnicity)
+        self.study_type = study_type  # Can be: case-control, trio, etc.
+
+    def __eq__(self, other):
+        return super(GWASData, self).__eq__(other)
+
+    def __hash__(self):
+        return super(GWASData, self).__hash__()
 
 
 class GWASSummaryStatisticsData(GWASData):
@@ -75,27 +100,36 @@ class DNASequencingData(Data):
         self.coverage_list = coverage_list
         self.genome_reference = genome_reference
 
+    def __eq__(self, other):
+        return super(DNASequencingData, self).__eq__(
+            other) and self.libraries == other.libraries and self.samples == other.samples and self.genomic_regions == other.genomic_regions
+
+    def __hash__(self):
+        return super(DNASequencingData, self).__hash__()
+
+
 class DNASequencingDataAsReads(DNASequencingData):
     def __init__(self, pmid_list, security_level=constants.SECURITY_LEVEL_2, processing=None, coverage_list=None,
                  sorting_order=None, genomic_regions=None, library_strategy=None, library_source=None, seq_centers=None,
                  genome_reference=None):
-        super(DNASequencingDataAsReads, self).__init__(pmid_list, security_level, processing=processing, genome_reference=genome_reference,
-                                              coverage_list=coverage_list, sorting_order=sorting_order,
-                                              library_strategy=library_strategy, library_source=library_source,
-                                              genomic_regions=genomic_regions)
+        super(DNASequencingDataAsReads, self).__init__(pmid_list, security_level, processing=processing,
+                                                       genome_reference=genome_reference,
+                                                       coverage_list=coverage_list, sorting_order=sorting_order,
+                                                       library_strategy=library_strategy, library_source=library_source,
+                                                       genomic_regions=genomic_regions)
         self.seq_centers = seq_centers
 
 
 class DNAVariationData(DNASequencingData):
     def __init__(self, pmid_list, security_level=constants.SECURITY_LEVEL_2, processing=None, coverage_list=None,
-                 sorting_order=None, genomic_regions=None, library_strategy=None, library_source=None, genome_reference=None):
+                 sorting_order=None, genomic_regions=None, library_strategy=None, library_source=None,
+                 genome_reference=None):
         super(DNAVariationData, self).__init__(pmid_list, security_level=security_level, coverage_list=coverage_list,
-                                               processing=processing, sorting_order=sorting_order, genomic_regions=genomic_regions,
+                                               processing=processing, sorting_order=sorting_order,
+                                               genomic_regions=genomic_regions,
                                                library_strategy=library_strategy, library_source=library_source,
                                                genome_reference=genome_reference)
-        # used_samtools = StringField()
-        # used_unified_genotyper = BooleanField()
-        #     used_haplotype_caller = BooleanField()
+
 
 class ArchiveData(Data):
     """
