5937901f5789f5d95c7bd29e1e62ea76f4aaee64
IrinaColgiu <ic4@sanger.ac.uk>
2016-10-20 10:54:48 +0100

    Rewritten the api, to be easier to use

--- serapis/storage/irods/api.py
+++ serapis/storage/irods/api.py
@@ -22,153 +22,178 @@ from serapis.storage.irods.baton import BatonCollectionAPI, BatonDataObjectAPI
 from serapis.storage.irods.icommands import ICmdsDataObjectAPI, ICmdsCollectionAPI
 
 
-class IrodsBasicAPI:
-    @classmethod
-    def get_acls(cls):
-        raise NotImplementedError()
+class API:
+    _BATON_CLASS_NAME = None
+    _ICMDS_CLASS_NAME = None
 
-    @classmethod
-    def add_or_replace_acl(cls):
-        raise NotImplementedError()
-
-    @classmethod
-    def add_or_replace_a_list_of_acls(cls):
-        raise NotImplementedError()
-
-    @classmethod
-    def remove_acl_for_user(cls):
-        raise NotImplementedError()
-
-    @classmethod
-    def remove_all_acls(cls):
-        raise NotImplementedError()
-
-    @classmethod
-    def remove_acls_for_a_list_of_users(cls):
-        raise NotImplementedError()
-
-    @classmethod
-    def get_all_metadata(cls):
-        raise NotImplementedError()
-
-    @classmethod
-    def remove_all_metadata(cls):
-        raise NotImplementedError()
-
-    @classmethod
-    def add_metadata(cls):
-        raise NotImplementedError()
-
-    @classmethod
-    def remove_metadata(cls):
-        raise NotImplementedError()
-
-    @classmethod
-    def update_metadata(cls):
-        raise NotImplementedError()
-
-    @classmethod
-    def upload(cls, src_path, dest_path):
-        raise NotImplementedError()
-
-    @classmethod
-    def copy(cls, src_path, dest_path):
-        raise NotImplementedError()
-
-    @classmethod
-    def move(cls, src_path, dest_path):
-        raise NotImplementedError()
-
-    @classmethod
-    def remove(cls, path):
-        raise NotImplementedError()
-
-
-
-
-
-class CollectionAPI(IrodsBasicAPI):
     @classmethod
     def get_acls(cls, path):
-        return BatonCollectionAPI.get_acls(path)
+        return cls._BATON_CLASS_NAME.get_acls(path)
 
     @classmethod
     def add_or_replace_acl(cls, path, acl):
-        return BatonCollectionAPI.add_or_replace_acl(path, acl)
+        return cls._BATON_CLASS_NAME.add_or_replace_acl(path, acl)
 
     @classmethod
     def add_or_replace_a_list_of_acls(cls, path, acls):
-        return BatonCollectionAPI.add_or_replace_a_list_of_acls(path, acls)
+        return cls._BATON_CLASS_NAME.add_or_replace_a_list_of_acls(path, acls)
 
     @classmethod
     def remove_acl_for_user(cls, path, username, zone):
-        return BatonCollectionAPI.remove_acl_for_user(path, username, zone)
+        return cls._BATON_CLASS_NAME.remove_acl_for_user(path, username, zone)
 
     @classmethod
     def remove_all_acls(cls, path):
-        return BatonCollectionAPI.remove_all_acls(path)
+        return cls._BATON_CLASS_NAME.remove_all_acls(path)
 
     @classmethod
     def remove_acls_for_a_list_of_users(cls, path, user_zone_tuples):
-        return BatonCollectionAPI.remove_acls_for_a_list_of_users(path, user_zone_tuples)
+        return cls._BATON_CLASS_NAME.remove_acls_for_a_list_of_users(path, user_zone_tuples)
 
     @classmethod
     def get_all_metadata(cls, path):
-        return BatonCollectionAPI.get_all_metadata(path)
+        return cls._BATON_CLASS_NAME.get_all_metadata(path)
 
     @classmethod
     def remove_all_metadata(cls, path):
-        return BatonCollectionAPI.remove_all_metadata(path)
+        return cls._BATON_CLASS_NAME.remove_all_metadata(path)
 
     @classmethod
     def add_metadata(cls, path, avu_dict):
-        return BatonCollectionAPI.add_metadata(path, avu_dict)
+        return cls._BATON_CLASS_NAME.add_metadata(path, avu_dict)
 
     @classmethod
     def remove_metadata(cls, path, avu_dict):
-        return BatonCollectionAPI.remove_metadata(path, avu_dict)
+        return cls._BATON_CLASS_NAME.remove_metadata(path, avu_dict)
 
     @classmethod
     def update_metadata(cls, path, key, new_values):
-        return BatonCollectionAPI.update_metadata(path, key, new_values)
+        return cls._BATON_CLASS_NAME.update_metadata(path, key, new_values)
 
     @classmethod
     def upload(cls, src_path, dest_path):
-        raise NotImplementedError()
+        return cls._ICMDS_CLASS_NAME.upload(src_path, dest_path)
 
     @classmethod
     def copy(cls, src_path, dest_path):
-        raise NotImplementedError()
+        return cls._ICMDS_CLASS_NAME.copy(src_path, dest_path)
 
     @classmethod
     def move(cls, src_path, dest_path):
-        raise NotImplementedError()
+        return cls._ICMDS_CLASS_NAME.move(src_path, dest_path)
 
     @classmethod
     def remove(cls, path):
-        raise NotImplementedError()
+        return cls._ICMDS_CLASS_NAME.remove(path)
+
+
+class CollectionAPI(API):
+    _BATON_CLASS_NAME = BatonCollectionAPI
+    _ICMDS_CLASS_NAME = ICmdsCollectionAPI
 
     @classmethod
-    def create(cls):
-        raise NotImplementedError()
+    def create(cls, path):
+        return cls._ICMDS_CLASS_NAME.create(path)
 
     @classmethod
     def list_data_objects(cls, path):
-        BatonCollectionAPI.list_data_objects(path)
+        return cls._BATON_CLASS_NAME.list_data_objects(path)
 
     @classmethod
-    def list_collection(cls, path):
-        BatonCollectionAPI.list_collections(path)
+    def list_collections(cls, path):
+        return cls._BATON_CLASS_NAME.list_collections(path)
 
 
-
-
-class DataObjectAPI(IrodsBasicAPI):
+class DataObjectAPI(API):
+    _BATON_CLASS_NAME = BatonDataObjectAPI
+    _ICMDS_CLASS_NAME = ICmdsDataObjectAPI
 
     @classmethod
-    def recalculate_checksum(cls):
-        raise NotImplementedError()
+    def recalculate_checksum(cls, path):
+        return cls._ICMDS_CLASS_NAME.recalculate_checksums(path)
 
     @classmethod
     def get_checksum(cls):
+        # TODO: implement this in BatonDataObjectAPI (missing atm)
         raise NotImplementedError()
+
+
+
+
+#
+# class CollectionAPI(IrodsBasicAPI):
+#     @classmethod
+#     def get_acls(cls, path):
+#         return BatonCollectionAPI.get_acls(path)
+#
+#     @classmethod
+#     def add_or_replace_acl(cls, path, acl):
+#         return BatonCollectionAPI.add_or_replace_acl(path, acl)
+#
+#     @classmethod
+#     def add_or_replace_a_list_of_acls(cls, path, acls):
+#         return BatonCollectionAPI.add_or_replace_a_list_of_acls(path, acls)
+#
+#     @classmethod
+#     def remove_acl_for_user(cls, path, username, zone):
+#         return BatonCollectionAPI.remove_acl_for_user(path, username, zone)
+#
+#     @classmethod
+#     def remove_all_acls(cls, path):
+#         return BatonCollectionAPI.remove_all_acls(path)
+#
+#     @classmethod
+#     def remove_acls_for_a_list_of_users(cls, path, user_zone_tuples):
+#         return BatonCollectionAPI.remove_acls_for_a_list_of_users(path, user_zone_tuples)
+#
+#     @classmethod
+#     def get_all_metadata(cls, path):
+#         return BatonCollectionAPI.get_all_metadata(path)
+#
+#     @classmethod
+#     def remove_all_metadata(cls, path):
+#         return BatonCollectionAPI.remove_all_metadata(path)
+#
+#     @classmethod
+#     def add_metadata(cls, path, avu_dict):
+#         return BatonCollectionAPI.add_metadata(path, avu_dict)
+#
+#     @classmethod
+#     def remove_metadata(cls, path, avu_dict):
+#         return BatonCollectionAPI.remove_metadata(path, avu_dict)
+#
+#     @classmethod
+#     def update_metadata(cls, path, key, new_values):
+#         return BatonCollectionAPI.update_metadata(path, key, new_values)
+#
+#     @classmethod
+#     def upload(cls, src_path, dest_path):
+#         return ICmdsCollectionAPI.upload(src_path, dest_path)
+#
+#     @classmethod
+#     def copy(cls, src_path, dest_path):
+#         return ICmdsCollectionAPI.copy(src_path, dest_path)
+#
+#     @classmethod
+#     def move(cls, src_path, dest_path):
+#         return ICmdsCollectionAPI.move(src_path, dest_path)
+#
+#     @classmethod
+#     def remove(cls, path):
+#         return ICmdsCollectionAPI.remove(path)
+#
+#     @classmethod
+#     def create(cls, path):
+#         return ICmdsCollectionAPI.create(path)
+#
+#     @classmethod
+#     def list_data_objects(cls, path):
+#         return BatonCollectionAPI.list_data_objects(path)
+#
+#     @classmethod
+#     def list_collection(cls, path):
+#         return BatonCollectionAPI.list_collections(path)
+#
+
+
+
