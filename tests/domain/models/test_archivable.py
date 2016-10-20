e5fab320e43b9485c3de96bd446ef2f3f46218ea
IrinaColgiu <ic4@sanger.ac.uk>
2016-10-20 14:44:02 +0100

    Created new test module for Archivable

---
+++ tests/domain/models/test_archivable.py
@@ -0,0 +1,81 @@
+"""
+Copyright (C) 2016  Genome Research Ltd.
+
+Author: Irina Colgiu <ic4@sanger.ac.uk>
+
+This program is part of serapis
+
+serapis is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+This file has been created on Oct 20, 2016.
+"""
+
+import os
+from os.path import basename, join
+import unittest
+
+from serapis.domain.models.archivable import ArchivableFile
+
+
+class ArchivableFileTest(unittest.TestCase):
+
+    def test_stage(self):
+        src_path = os.path.realpath(__file__)
+        src_fname = basename(src_path)
+        dest_path = '/humgen/projects/serapis_staging/test-archivable'
+        file = ArchivableFile(src_path, dest_path)
+        file.stage()
+
+        # testing that it all worked:
+
+
+
+#
+# class ArchivableFile(Archivable):
+#
+#     def __init__(self, src_path, dest_path, file_obj):
+#         self.src_path = src_path
+#         self.dest_path = dest_path
+#         self.file_obj = file_obj
+#
+#     def _upload(self):
+#         DataObjectAPI.upload(self.src_path, self.dest_path)
+#
+#     def _gather_metadata(self):
+#         self.file_obj.gather_metadata()
+#
+#     def stage(self):
+#         self._upload()
+#         self._gather_metadata()
+#         self._save_metadata_to_db()
+#
+#     def archive(self):
+#         #check if metadata enough
+#         # add metadata to the staged file
+#         DataObjectAPI.move(self.src_path, self.dest_path)
+#
+#     def __eq__(self, other):
+#         return type(self) == type(other) and self.src_path == other.src_path and \
+#                self.dest_path == other.dest_path and self.file_obj == other.file_obj
+#
+#     def __hash__(self):
+#         return hash(self.src_path) + hash(self.dest_path)
+
+
+
+
+
+
+
+
+
+
