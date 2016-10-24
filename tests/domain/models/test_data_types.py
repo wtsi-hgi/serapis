481343f2ddfbe59ed836b1ec2bbd0d39eb218760
IrinaColgiu <ic4@sanger.ac.uk>
2016-10-24 16:23:24 +0100

    Added more tests for the data types

--- tests/domain/models/test_data_types.py
+++ tests/domain/models/test_data_types.py
@@ -19,50 +19,66 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 This file has been created on Oct 24, 2016.
 """
 
+import unittest
+
+from serapis.domain.models.data_types import Data, DNASequencingData, GenotypingData, GWASSummaryStatisticsData
 
 
+class DataTest(unittest.TestCase):
+
+    def test_get_missing_fields_when_ok(self):
+        data = Data(pmid_list=[1234], studies="Some Study")
+        result = data._get_missing_fields(data._mandatory_fields)
+        self.assertListEqual([], result)
+
+    def test_get_missing_fields_when_missing_one(self):
+        data = Data(pmid_list=[1234])
+        result = data._get_missing_fields(data._mandatory_fields)
+        self.assertEqual(len(result), 1)
+
+    def test_get_missing_fields_when_all_missing(self):
+        data = Data(security_level=None)
+        result = data._get_missing_fields(data._mandatory_fields)
+        self.assertEqual(len(result), 2)
+
+    def test_has_enough_metadata_when_ok(self):
+        data = Data(studies="Some Study")
+        self.assertTrue(data.has_enough_metadata())
+
+    def test_get_all_missing_fields_when_all_missing(self):
+        data = Data(security_level=None)
+        result = data.get_all_missing_fields()
+        self.assertEqual(len(result), 4)
+
+    def test_get_missing_mandatory_fields(self):
+        data = Data(security_level=None)
+        result = data.get_missing_mandatory_fields()
+        self.assertEqual(len(result), 2)
 
 
-# class Data(object):
-#     """
-#         This is a generic type for any kind of data to be archived. Holds general attributes.
-#     """
-#     def __init__(self, processing=None, pmid_list=None, studies=None, security_level=constants.SECURITY_LEVEL_2):
-#         self.processing = processing
-#         self.pmid_list = pmid_list
-#         self.security_level = security_level
-#         self.studies = studies
-#
-#     @property
-#     def _mandatory_fields(self):
-#         return ['security_level', 'studies']
-#
-#     @property
-#     def _all_fields(self):
-#         return ['security_level', 'studies', 'processing', 'pmid_list']
-#
-#     def _get_missing_fields(self, field_names):
-#         missing_fields = []
-#         for field in self._all_fields:
-#             if not getattr(self, field):
-#                 missing_fields.append(field)
-#         return missing_fields
-#
-#     def has_enough_metadata(self):
-#         missing_mandatory_fields = self._get_missing_fields(self._mandatory_fields)
-#         return True if not missing_mandatory_fields else False
-#         # for field in self._mandatory_fields:
-#         #     if not getattr(self, field):
-#         #         return False
-#         # return True
-#
-#     def get_all_missing_fields(self):
-#         return self._get_missing_fields(self._all_fields)
-#         # missing_fields = []
-#         # for field in self._all_fields:
-#         #     if not getattr(self, field):
-#         #         missing_fields.append(field)
-#         # return missing_fields
-#
-#     def get_missing_mandatory_fields(self):
-#         return self._get_missing_fields(self._mandatory_fields)
+class GenotypingDataTest(unittest.TestCase):
+
+    def test_get_missing_mandatory_fields_when_all_missing(self):
+        data = GenotypingData()
+        result = data.get_missing_mandatory_fields()
+        self.assertEqual(len(result), 4)
+
+    def test_get_missing_mandatory_fields_when_nothing_missing(self):
+        data = GenotypingData(disease_or_trait='IBD', studies='IBD', nr_samples=33, genome_reference='B37')
+        result = data.get_missing_mandatory_fields()
+        self.assertEqual(len(result), 0)
+
+
+class DNASequencingDataTest(unittest.TestCase):
+
+    def test_get_missing_mandatory_fields_when_missing(self):
+        data = DNASequencingData()
+        result = data.get_missing_mandatory_fields()
+        self.assertEqual(len(result), 3)
+
+    def test_get_missing_mandatory_fields_when_ok(self):
+        data = DNASequencingData(studies=['IBD'], samples=['s1', 's2'], coverage_list=['2x'])
+        result = data.get_missing_mandatory_fields()
+        self.assertEqual(len(result), 0)
+        self.assertTrue(data.has_enough_metadata())
+
