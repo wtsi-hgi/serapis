823f351480199a90a9e20d66fd303dd298c8e3b4
IrinaColgiu <ic4@sanger.ac.uk>
2016-10-28 15:14:30 +0100

    Added tests for fct converting tuples2lists

--- tests/domain/models/test_archivable.py
+++ tests/domain/models/test_archivable.py
@@ -22,6 +22,7 @@ This file has been created on Oct 20, 2016.
 import os
 from os.path import basename, join
 import unittest
+from collections import defaultdict
 
 from serapis.domain.models.file import SerapisFile
 from serapis.storage.irods.api import DataObjectAPI
@@ -30,6 +31,8 @@ from serapis.domain.models.file_formats import BAMFileFormat
 from serapis.domain.models.data_type_mapper import DataTypeNames
 
 
+
+@unittest.skip
 class ArchivableFileTest(unittest.TestCase):
     def setUp(self):
         self.src_path = os.path.realpath(__file__)
@@ -50,28 +53,54 @@ class ArchivableFileTest(unittest.TestCase):
         self.archivable.unstage()
 
 
+class ArhivableFileTest(unittest.TestCase):
+
+    def test_from_tuples_to_dict_when_ok(self):
+        tuples_list = [('sample', 1), ('sample', 2)]
+        expected = defaultdict(set)
+        expected['sample'] = set([1, 2])
+        result = ArchivableFile._from_tuples_to_dict(tuples_list)
+        self.assertDictEqual(expected, result)
+
+    def test_from_tuples_to_dict_when_empty(self):
+        tuples_list = []
+        expected = defaultdict(set)
+        result = ArchivableFile._from_tuples_to_dict(tuples_list)
+        self.assertDictEqual(expected, result)
+
+    def test_from_tuples_to_dict_when_more_keys(self):
+        tuples_list = [('sample', 1), ('sample', 2), ('library', 1), ('study', 1)]
+        expected = defaultdict(set)
+        expected['sample'] = set([1, 2])
+        expected['library'] = set([1])
+        expected['study'] = set([1])
+        result = ArchivableFile._from_tuples_to_dict(tuples_list)
+        self.assertDictEqual(expected, result)
+
+    def test_from_tuples_to_dict_when_None(self):
+        tuples_list = [('sample', None), ('library', 1)]
+        expected = defaultdict(set)
+        expected['library'] = set([1])
+        result = ArchivableFile._from_tuples_to_dict(tuples_list)
+        self.assertDictEqual(expected, result)
+
+
 
 # class ArchivableFile(Archivable):
-#
+#     """
+#     This class is an interface for any type of file to be archived (ie stored in a specific location within iRODS
+#     humgen, with a bare minimum of metadata attached to it.
+#     """
 #     def __init__(self, src_path, dest_dir, file_obj=None):
 #         super(ArchivableFile, self).__init__(src_path, dest_dir)
 #         self.file_obj = file_obj
 #
-#     @property
-#     def dest_path(self):
-#         return os.path.join(self.dest_dir, os.path.basename(self.src_path))
-#
-#     def stage(self):
-#         DataObjectAPI.upload(self.src_path, self.dest_dir)
-#         self.file_obj.gather_metadata(self.src_path)
-#
-#     def unstage(self):
-#         DataObjectAPI.remove(self.dest_path)
-#
-#     def archive(self):
-#         #check if metadata enough
-#         # add metadata to the staged file
-#         DataObjectAPI.move(self.src_path, self.dest_dir)
+#     @classmethod
+#     def _from_tuples_to_dict(cls, tuples_list):
+#         result = defaultdict(set())
+#         for tup in tuples_list:
+#             result[tup[0]].add(tup[1])
+#         return result
 #
 
 
