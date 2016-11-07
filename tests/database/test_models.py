3eb3cbc67301abc8dd8e4fc81afc8dd82967cd3b
IrinaColgiu <ic4@sanger.ac.uk>
2016-11-07 12:06:55 +0000

    Added more unittests

--- tests/database/test_models.py
+++ tests/database/test_models.py
@@ -60,3 +60,96 @@ class SampleTests(unittest.TestCase):
         self.assertEqual(s1, s2)
 
 
+class LibraryTests(unittest.TestCase):
+    def test_eq_when_not_eq(self):
+        lib1 = Library(name='lib1')
+        lib2 = Library()
+        self.assertNotEqual(lib1, lib2)
+
+    def test_eq_when_not_eq2(self):
+        lib1 = Library(name='lib1')
+        lib2 = Library(name='lib2')
+        self.assertNotEqual(lib1, lib2)
+
+    def test_eq_when_eq(self):
+        lib1 = Library()
+        lib2 = Library()
+        self.assertEqual(lib1, lib2)
+
+    def test_eq_when_eq2(self):
+        lib1 = Library(name='lib1')
+        lib2 = Library(name='lib1')
+        self.assertEqual(lib1, lib2)
+
+
+class StudyTests(unittest.TestCase):
+    def test_eq_when_not_eq(self):
+        std1 = Study(name='std1')
+        std2 = Study(name='std2')
+        self.assertNotEqual(std1, std2)
+
+    def test_eq_when_not_eq2(self):
+        std1 = Study(name='std1')
+        std2 = Study()
+        self.assertNotEqual(std1, std2)
+
+    def test_eq_when_eq(self):
+        std1 = Study(name='std1', accession_number='ega1', internal_id=1)
+        std2 = Study(name='std1', accession_number='ega1', internal_id=1)
+        self.assertEqual(std2, std1)
+
+
+class DataTests(unittest.TestCase):
+
+    def test_eq_when_not_eq(self):
+        data1 = Data()
+        data1.pmid_list = [1,2]
+        data2 = Data()
+        data2.studies = [Study()]
+        self.assertNotEqual(data1, data2)
+
+    def test_eq_when_eq(self):
+        data1 = Data()
+        data1.pmid_list = [1,2]
+        data2 = Data()
+        data2.pmid_list = [1,2]
+        self.assertEqual(data1, data2)
+
+
+class DNASequencingDataTests(unittest.TestCase):
+
+    def test_eq_when_not_eq1(self):
+        data1 = DNASequencingData()
+        data1.samples = [Sample(name='s1')]
+        data2 = DNASequencingData()
+        data2.samples = [Sample(name='s2')]
+        self.assertNotEqual(data1, data2)
+
+    def test_eq_when_not_eq2(self):
+        data1 = DNASequencingData()
+        data1.samples = [Sample(name='s2', accession_number='EGA1')]
+        data2 = DNASequencingData()
+        data2.samples = [Sample(name='s2')]
+        self.assertNotEqual(data1, data2)
+
+    def test_eq_when_not_eq3(self):
+        data1 = DNASequencingData()
+        data1.samples = [Sample(name='s2')]
+        data1.libraries = [Library(internal_id=123)]
+        data2 = DNASequencingData()
+        data2.samples = [Sample(name='s2')]
+        self.assertNotEqual(data1, data2)
+
+    def test_eq_when_eq(self):
+        data1 = DNASequencingData()
+        data1.samples = [Sample(name='s1')]
+        data2 = DNASequencingData()
+        data2.samples = [Sample(name='s1')]
+        self.assertEqual(data1, data2)
+
+
+
+
+
+
+
